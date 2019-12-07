ec2211c4a3d7d1de02c5063db6b1980b38138387
supharada1 <58135541+supharada1@users.noreply.github.com>
2019-12-07 15:25:14 +0700

    Add files via upload

---
+++ Exercise4_Supharada_W.py
@@ -0,0 +1,10 @@
+FoE = (85)
+GeB = (70)
+Inc = (89)
+Cop = (90)
+
+print("----Your Score----")
+print("Foundation English :" , FoE )
+print("General Business   :" ,GeB)
+print("Introduction to Computer Systems :",Inc)
+print("Computer Programming :",Cop)
