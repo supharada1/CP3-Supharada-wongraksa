0915a7eec07614b1e3dc5b910f55b8d41c271a82
supharada1 <58135541+supharada1@users.noreply.github.com>
2019-12-08 16:40:12 +0700

    Add files via upload

---
+++ Exercise5_1_Supharada_W.py
@@ -0,0 +1,6 @@
+N1 = int(input())
+N2 =int(input())
+print(N1+N2)
+print(N1-N2)
+print(N1*N2)
+print(N1/N2)
---
+++ Exercise5_2_Supharada_W.py
@@ -0,0 +1,4 @@
+V = int(input("ระยะทาง : " ))
+t = int (input("เวลาที่เคลื่อนที่ : " ))
+calculate = V/t
+print(calculate,"km/h")
